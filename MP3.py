bff724ff41f0d7de0f1be697112075a4bdf2a4d6
Rubén Tostón Carrero <ruben.toston@edu.uah.es>
2025-07-14 22:43:00 +0200

    Youtube to mp3 downloader using ffmpeg and yt_dlp added new feature: it can download more than 1 video at a time

--- MP3.py
+++ MP3.py
@@ -1,4 +1,5 @@
 import yt_dlp
+import time
 
 def descargar_mp3_con_ytdlp(url, carpeta_destino):
     opciones = {
@@ -13,6 +14,19 @@ def descargar_mp3_con_ytdlp(url, carpeta_destino):
     with yt_dlp.YoutubeDL(opciones) as ydl:
         ydl.download([url])
 
-# Ejemplo de uso
-url = input("Introduce la URL del video de YouTube: ")
-descargar_mp3_con_ytdlp(url, 'E:/Music/XD')
+
+lista=[]
+e=1
+while True:
+    print(f"{e}º video:")
+    url = input("Introduce la URL del video de YouTube (enter para terminar de añadir): ")
+    if url:#no está vacio
+        lista.append(url)
+        e+=1
+    else:
+        break
+for i,enlace in enumerate(lista, start=1):
+    descargar_mp3_con_ytdlp(enlace, 'E:/Music/XD')
+    print(f"\nVideo {i}/{len(lista)} descargado. Siguiente...")
+    time.sleep(3)
+print("Proceso finalizado")
